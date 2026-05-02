7f2001535b5ccb53ce4c27c1aaeea386b32ae46d
BaskinB <54458253+BaskinB@users.noreply.github.com>
2026-05-02 16:30:32 -0500

    chore(manifest): version bump.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
 game("gta5")
 lua54("yes")
 fx_version("cerulean")
-version("1.0.0")
+version "1.0.1"
 client_script("@pulsar-core/exports/cl_error.lua")
 client_script("@pulsar-pwnzor/client/check.lua")
 
